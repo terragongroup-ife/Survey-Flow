b0f63c77f19494bb3db61853ba0a79339f8ea62e
Mubarak Bello <bellomuba.rak0@gmail.com>
2019-03-14 11:41:17 +0100

    Fixed api call bug

--- src/components/CreateSurvey.jsx
+++ src/components/CreateSurvey.jsx
@@ -146,7 +146,7 @@ class CreateSurvey extends Component {
     surveyData['surveyQuestions'] = this.state.questions
 
     // Now, post the survey to the backend
-    fetch('https://young-anchorage-24773.herokuapp.com/post-survey', {
+    fetch('https://young-anchorage-24773.herokuapp.com/questions', {
       headers: new Headers({
         'Accept': 'application/json',
         'Content-Type': 'application/json'
